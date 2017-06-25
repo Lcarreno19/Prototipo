3828bdb388c6219c4e0ad5e7ae872c0777dedbd3
Luis Daniel Carreño <luiscarreno1991@gmail.com>
2017-06-25 18:40:17 -0300

    Codigo Editado

--- html/index/index.php
+++ html/index/index.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 	<html>
 			<head>
-				<?php include_once("html/overall/header.php")?>
-              <!-- Styles -->
+				<?php include_once("html/overall/header.php");?>
         <style>
           #chartdiv {
               width: 100%;
@@ -23,10 +22,12 @@
         <script src="https://www.amcharts.com/lib/3/plugins/export/export.min.js"></script>
         <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
         <script src="https://www.amcharts.com/lib/3/themes/light.js"></script>
+        <script src="//www.amcharts.com/lib/3/plugins/dataloader/dataloader.min.js"></script>
+
 			</head>
 		<body class="rtl">
 			<section id="menu-0">
-				<?php include_once("html/overall/topnav.php")?>
+				<?php include_once("html/overall/topnav.php");?>
 			</section>
 			<section class="mbr-info mbr-info-extra mbr-section mbr-section-md-padding" id="msg-box1-0" style="background-color: rgb(242, 242, 242); padding-top: 10px; padding-bottom: 40px;">
     			<div class="container">
@@ -99,13 +100,8 @@
 			<footer>
 				<?php include_once("html/overall/footer.php");
 				include_once("core/bin/functions/Consultores.php");?>
-  <!--       <script type="text/javascript" src="views/app/grafico2.js"></script> -->
-        <script type="text/javascript" src="views/app/grafico.js"></script>
-
-<!--         <script type="text/javascript" src="views/assets/prototipo/jquery-3.2.1.min,js"></script>
- -->				<script type="text/javascript">
+        <script type="text/javascript">
         $('#loading-image').hide(); 
-
 	         function Consulta1(){
             $('#chartdiv').css('display', 'none');
             $('#chartdiv2').css('display', 'none');
@@ -130,69 +126,120 @@
                 });
             }
 
-                      function Consulta2(){
-                        $('#chartdiv').css('display', 'block');
-                        $('#chartdiv2').css('display', 'none');
-                        $('#creartablaprecio').css('display', 'none');
-                          var codigo = $('#SeleccionVendedor').val();
-                          var fecha1 = $('#fecha1').val();
-                          var fecha2 = $('#fecha2').val();
-                          var dataString = 'codigo='+ codigo + '&fecha1=' + fecha1 + '&fecha2=' + fecha2;
-                          var valores = '';
-                              $.ajax({
-                               type: "POST",
-                               url: "ajax.php?mode=consulta3",
-                               data: dataString,
-                              beforeSend: function(){
-                                $('#loading-image').show();
+            function Consulta2(){
+              $('#chartdiv').css('display', 'block');
+              $('#chartdiv2').css('display', 'none');
+              $('#creartablaprecio').css('display', 'none');
+              var codigo = $('#SeleccionVendedor').val();
+              var fecha1 = $('#fecha1').val();
+              var fecha2 = $('#fecha2').val();
+              var dataString = 'codigo='+ codigo + '&fecha1=' + fecha1 + '&fecha2=' + fecha2;
+                $.ajax({
+                  type: "POST",
+                  url: "ajax.php?mode=consulta2",
+                  data: dataString,
+                    beforeSend: function(){
+                      $('#loading-image').show();
+                    },
+                    complete: function(){
+                      $('#loading-image').hide();
+                    },
+                success: function(data){
+                      var valores = eval(data);
+                      var ene = valores[0];
+                      var feb = valores[1];
+                      var marz = valores[2];
+                      var abril = valores[3];
+                      var mayo = valores[4];
+                      var jun = valores[5];
+                      var jul = valores[6];
+                      var ago = valores[7];
+                      var sept = valores[8];
+                      var oct = valores[9];
+                      var nov = valores[10];
+                      var dic = valores[11];
+                      
+                        var chart = AmCharts.makeChart( "chartdiv", {
+                          "type": "serial",
+                          "theme": "light",
+                          "dataProvider": [ {
+                            "mes": "Ene",
+                            "total": ene
+                          }, {
+                            "mes": "Feb",
+                            "total": feb
+                          }, {
+                            "mes": "Mar",
+                            "total": marz
+                          }, {
+                            "mes": "Abr",
+                            "total": abril
+                          }, {
+                            "mes": "May",
+                            "total": mayo
+                          }, {
+                            "mes": "Jun",
+                            "total": jun
+                          }, {
+                            "mes": "Jul",
+                            "total": jul
+                          }, {
+                            "mes": "Ago",
+                            "total": ago
+                          }, {
+                            "mes": "Sep",
+                            "total": sept
+                          }, {
+                            "mes": "Oct",
+                            "total": oct
+                          }, {
+                            "mes": "Nov",
+                            "total": nov
+                          },{
+                            "mes": "Dic",
+                            "total": dic
+                          } ],
+                          "valueAxes": [ {
+                              "gridColor": "#FFFFFF",
+                              "gridAlpha": 0.2,
+                              "dashLength": 0
+                          } ],
+                              "gridAboveGraphs": true,
+                              "startDuration": 1,
+                              "graphs": [ {
+                                  "balloonText": "[[category]]: <b>[[value]]</b>",
+                                  "fillAlphas": 0.8,
+                                  "lineAlpha": 0.2,
+                                  "type": "column",
+                                  "valueField": "total"
+                                } ],
+                                "chartCursor": {
+                                  "categoryBalloonEnabled": false,
+                                  "cursorAlpha": 0,
+                                  "zoomable": false
                                 },
-                              complete: function(){
-                                $('#loading-image').hide();
+                                  "categoryField": "mes",
+                                  "categoryAxis": {
+                                    "gridPosition": "start",
+                                    "gridAlpha": 0,
+                                    "tickPosition": "start",
+                                    "tickLength": 20
                                 },
-                              success: function(data){
-                                var chart = AmCharts.makeChart( "chartdiv", {
-                                  "type": "serial",
-                                  "theme": "light",
-                                  "dataProvider": [{"mes": data[0],
-                                                   "total": data[1]}],
-                                  "gridAboveGraphs": true,
-                                  "startDuration": 1,
-                                  "graphs": [ {
-                                        "balloonText": "[[category]]: <b>[[value]]</b>",
-                                        "fillAlphas": 0.8,
-                                        "lineAlpha": 0.2,
-                                        "type": "column",
-                                        "valueField": "total"
-                                      } ],
-                                    "chartCursor": {
-                                      "categoryBalloonEnabled": false,
-                                      "cursorAlpha": 0,
-                                      "zoomable": false
-                                    },
-                                    "categoryField": "mes",
-                                    "categoryAxis": {
-                                      "gridPosition": "start",
-                                      "gridAlpha": 0,
-                                      "tickPosition": "start",
-                                      "tickLength": 20
-                                    },
-                                    "export": {
+                                  "export": {
                                       "enabled": true
                                     }
-                                  } );
-                                }
-                              });
-                          }
 
+                          } );
+                      }
+                    });
+          }
                        function Consulta3(){
                         $('#chartdiv').css('display', 'none');
                         $('#chartdiv2').css('display', 'block');
                           $('#creartablaprecio').css('display', 'none');
-                          var codigo = $('#SeleccionVendedor').val();
                           var fecha1 = $('#fecha1').val();
                           var fecha2 = $('#fecha2').val();
-                          var dataString = 'codigo='+ codigo + '&fecha1=' + fecha1 + '&fecha2=' + fecha2;
-                          var valores = '';
+                          var dataString = 'fecha1=' + fecha1 + '&fecha2=' + fecha2;
                               $.ajax({
                                type: "POST",
                                url: "ajax.php?mode=consulta3",
@@ -204,15 +251,23 @@
                                 $('#loading-image').hide();
                                 },
                               success: function(data){
+                                var consultor = data['Nombre'];
+                                var total = data['total'];
                                 var chart = AmCharts.makeChart("chartdiv2", {
                                   "type": "pie",
                                   "theme": "light",
                                   "innerRadius": "40%",
                                   "gradientRatio": [-0.4, -0.4, -0.4, -0.4, -0.4, -0.4, 0, 0.1, 0.2, 0.1, 0, -0.2, -0.5],
-                                  "dataProvider": [data],
+                                  "dataProvider": [{
+                                    "vendedor": "Consultor",
+                                    "total": 1234
+                                  },{
+                                    "vendedor": "Consultor2",
+                                    "total" : 1222
+                                  }],
                                   "balloonText": "[[value]]",
                                   "valueField": "total",
-                                  "titleField": "mes",
+                                  "titleField": "vendedor",
                                   "balloon": {
                                       "drop": true,
                                       "adjustBorderColor": false,
